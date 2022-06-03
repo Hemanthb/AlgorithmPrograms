a6e0563fb7005a05058e2f8abc40a4716f2ec7b1
hem_b <hemanthb93@gmail.com>
2022-06-03 18:31:26 +0530

    [Hemanth]:Permutation of a given string is displayed

---
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -0,0 +1,6 @@
+﻿AlgorithmPrograms.StringPermutation permutation = new AlgorithmPrograms.StringPermutation();
+String word = "ABC";
+int len = word.Length;
+Console.WriteLine("Permutation of given string {0} are -\n",word);
+AlgorithmPrograms.StringPermutation.Permute(word, 0, len - 1);
+Console.WriteLine();
---
+++ AlgorithmPrograms/AlgorithmPrograms/StringPermutation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    internal class StringPermutation
+    {
+        
+            public static void Permute(String str, int start, int len)
+            {
+                if (start == len)
+                    Console.Write(str+" ");
+                else
+                {
+                    for (int i = start; i <= len; i++)
+                    {
+                        str = Swap(str, start, i);
+                        Permute(str, start + 1, len);
+                        str = Swap(str, start, i);
+                    }
+                }
+            }
+
+            public static String Swap(String a,
+                                    int i, int j)
+            {
+                char temp;
+                char[] charArray = a.ToCharArray();
+                temp = charArray[i];
+                charArray[i] = charArray[j];
+                charArray[j] = temp;
+                string s = new string(charArray);
+                return s;
+            }
+
+        }
+
+}
