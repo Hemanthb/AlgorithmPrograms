7569a0013045ee7b39dc681e6dac80119f43ff0a
hem_b <hemanthb93@gmail.com>
2022-06-04 00:08:00 +0530

    [Hemanth]:Binary search of a given word among array of words from a file

---
+++ AlgorithmPrograms/AlgorithmPrograms/BinarySearch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class BinarySearch
+    {
+        public void SearchWord(String[] wordArray,int start,int end,string word)
+        {
+            if (end >= start)
+            {
+                int midPt = start + (end - start)/ 2;
+                string midword = wordArray[midPt];
+
+                if (midword.Equals(word))
+                {
+                    Console.WriteLine("Required word - {0} is found", word);
+                    return;
+                }
+                if (midword.CompareTo(word)>0)
+                {
+                    SearchWord(wordArray, 0, midPt - 1, word);
+                    return;
+
+                }
+                if (midword.CompareTo(word) < 0)
+                {
+                    SearchWord(wordArray, midPt + 1, end, word);
+                    return;
+
+                }
+               
+            }
+            Console.WriteLine("Required word - {0} is not found", word);
+            return;
+        }
+    }
+}
--- AlgorithmPrograms/AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -1,6 +1,26 @@
 ﻿AlgorithmPrograms.StringPermutation permutation = new AlgorithmPrograms.StringPermutation();
-String word = "ABC";
-int len = word.Length;
-Console.WriteLine("Permutation of given string {0} are -\n",word);
-AlgorithmPrograms.StringPermutation.Permute(word, 0, len - 1);
-Console.WriteLine();
+int choice = 2;
+switch (choice) 
+{
+    case 1:
+        String word = "ABC";
+        int len = word.Length;
+        Console.WriteLine("Permutation of given string {0} are -\n",word);
+        AlgorithmPrograms.StringPermutation.Permute(word, 0, len - 1);
+        Console.WriteLine();
+        break;
+    case 2:
+        string fileName = @"D:\blabz_fellowship\AlgorithmPrograms\AlgorithmPrograms\AlgorithmPrograms\wordlist.txt";
+        string fileContents = "";
+        if (File.Exists(fileName))
+        {
+            fileContents = File.ReadAllText(fileName);
+        }
+        Console.WriteLine("ENter a word to check whther it exists in the file");
+        string search = Console.ReadLine();
+        AlgorithmPrograms.BinarySearch binarySearch = new AlgorithmPrograms.BinarySearch();
+        string[] wordArray = fileContents.ToLower().Split(' ');
+        Array.Sort(wordArray);
+        binarySearch.SearchWord(wordArray, 0, (wordArray.Length-1), search);
+        break;   
+}
